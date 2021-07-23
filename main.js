970cbd65faa2d397026f83c1ffa78693035ef40e
AyalaRazo <84156455+AyalaRazo@users.noreply.github.com>
2021-07-23 01:19:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let navToggle = document.querySelector('.toggle')
+let menuToggle = document.querySelector('.nav-main')
+
+navToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle('active');
+    navToggle.classList.toggle('active')
+})
